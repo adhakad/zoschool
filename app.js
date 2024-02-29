b0bf6e348b8b3826608893f71389e5e45ebe1471
Abhishek Dhakad <dhakaddeepak9340700360@gmail.com>
2024-02-29 10:10:28 +0530

    app.js update

--- app.js
+++ app.js
@@ -16,20 +16,20 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
 DbConnect();
-// app.use(function (req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
-//   next();
-// });
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  next();
+});
 app.use('/public', express.static('public'));
-// app.use(express.static(path.join(__dirname, './dist/zoclass')));
-// app.use('/',express.static(path.join(__dirname,'./dist/zoclass')));
+app.use(express.static(path.join(__dirname, './dist/zoclass')));
+app.use('/',express.static(path.join(__dirname,'./dist/zoclass')));
 
 require('./routes')(app);
 
-// app.get('/*',(req,res) => {
-//   res.sendFile(path.join(__dirname ,'./dist/zoclass/index.html'));
-// })
+app.get('/*',(req,res) => {
+  res.sendFile(path.join(__dirname ,'./dist/zoclass/index.html'));
+})
 
 
 
